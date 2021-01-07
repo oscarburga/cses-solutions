7ff994e2c06bca401a30e8cb4d86a10e065b8f4f
oscarburga <oscarburga2001@gmail.com>
2021-01-06 19:30:46 -0500

    removed leftover testing code

--- geometry/convex-hull.cpp
+++ geometry/convex-hull.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-
+ 
 const int maxn = 2e5+5; 
-
+ 
 int n;
 struct pt {
 	int x, y;
@@ -13,17 +13,17 @@ struct pt {
 	bool operator == (pt p) { return x == p.x && y == p.y; }
 } pts[maxn];
 vector<pt> up, lo;
-
+ 
 int main(){
 	scanf("%d", &n);
 	for(int i = 0; i<n; i++) scanf("%d %d", &pts[i].x, &pts[i].y);
+	//For this problem you should include collinear points in the convex hull, even though
+	//it's not mentioned in the statement. Costed me 2 WAs.
 	sort(pts, pts+n, [&](pt &p, pt &q) { return p.x < q.x || (p.x == q.x && p.y < q.y); });
 	up.reserve(maxn);
 	lo.reserve(maxn);
 	int l1 = 0, l2 = 0;
-	//For this problem you should include collinear points in the convex hull, even though
-	//it's not mentioned in the statement. Costed me 2 WAs.
-	for(int i = 0; i<n; i++) { 
+	for(int i = 0; i<n; i++) {
 		while(l1 > 1 && (up[l1-1]-up[l1-2]) % (pts[i]-up[l1-2]) > 0) up.pop_back(), --l1;
 		up.emplace_back(pts[i]);
 		l1++;
@@ -35,12 +35,7 @@ int main(){
 	lo.pop_back();
 	lo.insert(lo.end(), up.begin(), up.end());
 	lo.pop_back();
-	ll area = 0;
-	int m = lo.size();
-	for(int i = 0; i<m; i++) area += lo[i] % lo[(i+1)%m];
-	printf("%lld\n", area);
 	printf("%zu\n", lo.size());
 	for(auto&p: lo) printf("%d %d\n", p.x, p.y);
 	return 0;
 }
-
